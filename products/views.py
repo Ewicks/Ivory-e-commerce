667b7d6892d5d371ad67322eb9aa800359bd5fed
Ewicks <ethanowicks9@gmail.com>
2022-10-18 10:05:50 +0000

    Customize admin

--- products/views.py
+++ products/views.py
@@ -4,8 +4,8 @@ from django.contrib import messages
 
 def all_products(request):
 
-    return render(request, 'products/products.html', context)
+    return render(request, 'products/products.html', {})
 
 
 def product_detail(request, product_id):
-    return render(request, 'products/product_detail.html', context)
+    return render(request, 'products/product_detail.html', {})
