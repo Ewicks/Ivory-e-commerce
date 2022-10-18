7763e1ce0a34976aeb29808ff9405db899d64d38
Ewicks <ethanowicks9@gmail.com>
2022-10-18 10:43:55 +0000

    Add cards in product.html

--- products/views.py
+++ products/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from .models import Product
 
 
 def all_products(request):
 
-    return render(request, 'products/products.html', {})
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
 
 
 def product_detail(request, product_id):
