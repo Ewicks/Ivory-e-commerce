f2f58e4d3a8a21b28d3f4867cc6accec67673f1a
Ewicks <ethanowicks9@gmail.com>
2022-10-21 17:53:27 +0000

    Add stripe elemenets

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Ltw1xH61YyoyZ0ileWOYoN7dNCRIcsZyfOGSdD46h4quz6RvZjTczDyGU3qhSVReuBWZfK2DK4Pms0e8DPd9wPf00xjGKHP6g',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
