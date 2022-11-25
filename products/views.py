2e55c7a82d95d2d5ce944fadb6540394a8e7683c
Ewicks <ethanowicks9@gmail.com>
2022-11-25 16:48:48 +0000

    Fix empty spaces in layout

--- products/views.py
+++ products/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.db.models.functions import Lower
-
+from django.core.paginator import Paginator
 from .models import Product, Category
 from .forms import ProductForm
 
@@ -16,6 +16,10 @@ def all_products(request):
     categories = None
     sort = None
     direction = None
+
+    p = Paginator(Product.objects.all(), 8)
+    page = request.GET.get('page')
+    productss = p.get_page(page)
 
     if request.GET:
         if 'sort' in request.GET:
@@ -53,6 +57,7 @@ def all_products(request):
         'search_term': query,
         'current_categories': categories,
         'current_sorting': current_sorting,
+        'productss': productss,
     }
 
     return render(request, 'products/products.html', context)
